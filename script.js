b66cc702fb8bc6290144c944bd6a8ba026838437
Lara <lara.salman01@hotmail.com>
2025-04-10 11:11:00 +0200

    update variable name

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 
   // TODO: Skapa funktionen för att hämta väderdata
   // Tips: Funktionen ska ta emot en parameter
+  function getWeather(city_input){
   
   // TODO: Hämta API-nyckeln från config.js
   // Tips: Använd API_KEY variabeln som är definierad i config.js
@@ -12,10 +13,15 @@ $(document).ready(function () {
   // Om det är tomt, visa ett felmeddelande i #error och avsluta funktionen
 
   // TODO: Skapa API-URL (använd backticks och infoga city och din API-nyckel)
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city_input}&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 
   // TODO: Gör ett anrop till OpenWeatherMap med $.get()
   // Tips: Använd url som du byggde ovan och logga svaret i konsolen
+
+  $.get(url, function(city_input, ) {
+    console.log("City: " + city_input + )
+  })
   // Om det lyckas:
   // - Läs ut: stadsnamn, väderbeskrivning, temperatur, landkod, ikon-id
   // Om anropet misslyckas:
@@ -26,7 +32,7 @@ $(document).ready(function () {
   // Tips: Använd mallsträngar (backticks) för att bygga HTML
 
   
-
+  }
   // TODO: Välj knappen och lyssna på klick
   // Tips: Använd .on("click", ...) på rätt element
   
